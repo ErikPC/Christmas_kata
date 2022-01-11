430eb3dadb4fb4e67844c2567b56ad5cc546256e
ErikPC <epardillo@cifpfboll.eu>
2022-01-11 23:46:21 +0100

    añadir test yatzy, three_of_a_kind, two_pair

--- Yatzee_refact/test_yatzy_from_scratch_refatorizado_erik.py
+++ Yatzee_refact/test_yatzy_from_scratch_refatorizado_erik.py
@@ -10,6 +10,10 @@ def test_chance():
     assert 15 == Yatzy.chance(1, 2, 3, 4, 5)
     assert 14 == Yatzy.chance(1, 1, 3, 3, 6)
     assert 21 == Yatzy.chance(4, 5, 5, 6, 1)
+@pytest.mark.yatzy
+def yatzy():
+    assert 16 == Yatzy.yatzy(4, 4, 4, 4, 4)
+    assert 0 == Yatzy.yatzy(1, 1, 3, 3, 6)
 
 @pytest.mark.ones
 def test_ones():
@@ -43,6 +47,17 @@ def test_sixes():
     assert 12 == Yatzy.sixes(4, 6, 5, 4, 6)
 @pytest.mark.four_of_a_kind
 def four_of_a_kind():
-    assert 16 == Yatzy.fours(1, 4, 4, 4, 4)
-    assert 0 == Yatzy.fours(1, 1, 3, 3, 6)
-    assert 25 == Yatzy.fours(4, 5, 5, 5, 5)
+    assert 16 == Yatzy.four_of_a_kind(1, 4, 4, 4, 4)
+    assert 0 == Yatzy.four_of_a_kind(1, 1, 3, 3, 6)
+    assert 25 == Yatzy.four_of_a_kind(4, 5, 5, 5, 5)
+
+@pytest.mark.three_of_a_kind
+def three_of_a_kind():
+    assert 12 == Yatzy.three_of_a_kind(1, 2, 4, 4, 4)
+    assert 9 == Yatzy.three_of_a_kind(1, 1, 3, 3, 3)
+    assert 15 == Yatzy.three_of_a_kind(4, 2, 5, 5, 5)
+@pytest.mark.two_pair
+def two_pair():
+    assert 8 == Yatzy.fours(1, 6, 2, 4, 4)
+    assert 2 == Yatzy.fours(1, 1, 3, 5, 6)
+    assert 10 == Yatzy.fours(4, 1, 6, 5, 5)
