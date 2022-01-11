f9ea38e7fb838554b2fcfa9655f9b6875fe8f613
ErikPC <epardillo@cifpfbmoll.eu>
2022-01-11 14:03:35 +0100

    añadido test de los metodos:  twos, threes, fours

--- Yatzee_refact/test_yatzy_from_scratch_refatorizado_erik.py
+++ Yatzee_refact/test_yatzy_from_scratch_refatorizado_erik.py
@@ -16,6 +16,21 @@ def test_ones():
     assert 4 == Yatzy.ones(1, 1 ,1 ,6 ,1 )
     assert 1 == Yatzy.ones(1, 6 ,6 ,6, 5)
     assert 5 == Yatzy.ones(1, 1, 1, 1, 1)
+@pytest.mark.twos
+def test_twos():
+    assert 4 == Yatzy.twos(2, 4, 4, 5, 2)
+    assert 6 == Yatzy.twos(2, 4, 6, 2, 2)
+    assert 8 == Yatzy.twos(2, 2, 6, 2, 2)
+@pytest.mark.threes
+def test_three():
+    assert 6 == Yatzy.threes(3, 3, 6, 1, 2)
+    assert 9 == Yatzy.chance(3, 1, 3, 3, 6)
+    assert 0 == Yatzy.chance(4, 5, 5, 6, 1)
+@pytest.mark.fours
+def test_fours():
+    assert 4 == Yatzy.chance(1, 2, 3, 4, 5)
+    assert 0 == Yatzy.chance(1, 1, 3, 3, 6)
+    assert 8 == Yatzy.chance(4, 5, 5, 4, 1)
 @pytest.fixture
 def inyector():
     # Es el setup de unittest o de JUnit
